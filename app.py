bd7e5aae52710b5de5fab0bb2dfbb9222e4914df
RZaNil <niloyzani2018@gmail.com>
2026-01-20 23:35:12 +0600

    initial backend files

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# data.txt থেকে সব লাইন পড়া
+def load_data():
+    with open("data.txt", "r", encoding="utf-8") as f:
+        return f.readlines()
+
+# user question এর সাথে data মিলানো
+def find_answer(question):
+    question = question.lower()
+    data = load_data()
+
+    for line in data:
+        if ":" in line:
+            key, value = line.split(":", 1)
+            if key.lower() in question:
+                return value.strip()
+
+    return "Sorry, I could not find that information in the university records."
+
+# Alexa webhook
+@app.route("/", methods=["POST"])
+def alexa_webhook():
+    body = request.json
+
+    try:
+        user_text = body["request"]["intent"]["slots"]["query"]["value"]
+    except:
+        user_text = ""
+
+    answer = find_answer(user_text)
+
+    return jsonify({
+        "version": "1.0",
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": answer
+            },
+            "shouldEndSession": True
+        }
+    })
+
+if __name__ == "__main__":
+    app.run()
